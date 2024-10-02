eb0dcdededeac5c3e5e7dba0dbacb7b705389152
unknown <nolascojoaops@gmail.com>
2024-10-02 18:40:36 -0300

    Fix install ws script

--- scripts/install-web-server.sh
+++ scripts/install-web-server.sh
@@ -2,20 +2,24 @@
 
 # Update the instance packages
 sudo yum update -y
-# Install Apache and MariaDB server 
-sudo yum install -y httpd mariadb105-server
-# Start Apache and MariaDB
+
+# Install Apache and PHP
+sudo yum install -y httpd php php-mysqlnd
+
+# Start Apache
 sudo systemctl start httpd
-sudo systemctl start mariadb
-# Enable Apache and MariaDB to start on boot
+
+# Enable Apache to start on boot
 sudo systemctl enable httpd
-sudo systemctl enable mariadb
-# Download index.php 
+
+# Download index.php
 wget -O /var/www/html/index.php https://raw.githubusercontent.com/nolascojoao/aws-rds-creation-lab/main/scripts/index.php
+
 # Substitute variables in index.php
 sudo sed -i "s/\$host = 'your-rds-endpoint.amazonaws.com';/\$host = 'YOUR_RDS_ENDPOINT';/" /var/www/html/index.php
 sudo sed -i "s/\$username = 'your_rds_username';/\$username = 'YOUR_RDS_USERNAME';/" /var/www/html/index.php
 sudo sed -i "s/\$password = 'your_rds_password';/\$password = 'YOUR_RDS_PASSWORD';/" /var/www/html/index.php
 sudo sed -i "s/\$dbname = 'your_database_name';/\$dbname = 'YOUR_DATABASE_NAME';/" /var/www/html/index.php
+
 # Restart Apache to apply changes
 sudo systemctl restart httpd
